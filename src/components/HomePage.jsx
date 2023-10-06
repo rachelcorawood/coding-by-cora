f597acb12fc3ef79f3fbeb6917de869c48aaf52f
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-10-06 14:50:17 -0700

    nav bar rough draft

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutMe from "./AboutMe";
+import Sitebar from "./Sitebar";
 
 export default function HomePage() {
   return (
     <div>
-      <AboutMe />
+      <Sitebar />
+      <AboutMe/>
     </div>
 
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import HomePage from './components/HomePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
