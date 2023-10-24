251f115763789353ae75bf5739c54f56aa1e3661
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-10-24 14:42:45 -0700

    slight script changes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -15,12 +15,12 @@ export default function AboutMe() {
         <img src={photo} alt="portfolio-photo" />
       </div>
 
-      <section className='bio-background'>
+
         <div className='bio'>
           <h1 className='bio-title'> Hi, I'm Cora. Nice to meet you</h1>
-          <h2 className='bio-paragraph'>Prior to freelancing as a Web Developer, I was an Administrative Assistant and Medical Receptionist for a celebrity-acclaimed plastic surgeon. This work required extreme organization and obsessive attention to detail because one wrong mistake on a medical file could lead to a potentially fatal error. These skills of being precise and solution-oriented under pressure would make me an ideal candidate to handle building your site with preparedness and ease. I've also acted and produced film for over fourteen years. I dealt with continuous hurdles and was nicknamed “the rock” for instilling patience in our cast, crew, and investors. My resourcefulness and problem-solving mindset, has helped numerous creative projects surpass the finish line and I would love to help you with your creative project. </h2>
+          <h2 className='bio-paragraph'>Prior to freelancing as a Web Developer, I was an Administrative Assistant and Medical Receptionist for a celebrity-acclaimed plastic surgeon. This work required extreme organization and obsessive attention to detail because one wrong mistake on a medical file could lead to a potentially fatal error. These skills of being precise and solution-oriented under pressure would make me an ideal candidate to handle building your site with preparedness and ease. I've also acted and produced film for over fourteen years. I dealt with continuous hurdles and was nicknamed “the rock” for instilling patience in our cast, crew, and investors. My resourcefulness and problem-solving mindset, has helped numerous creative projects surpass the finish line and I would love to help yours. </h2>
         </div>
-      </section>
+    
 
 
 
