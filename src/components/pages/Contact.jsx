5b7350de2d22906baf8ba1547670f639a8f85d95
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-12-06 17:24:14 -0800

    contact form added

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,65 @@
-import React  from "react";
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import './Contact.css'
+
+export default function Contact() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const serviceId = "service_f39pio4";
+    const templateId = "contact_form";
+    const publicKey = "8x-DIm1tli9O5PUgW";
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "Web Wizard",
+      message: message,
+    }
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log('Thank you for reaching out! Email sent successfully!', response);
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      })
 
 
-export default function Contact (){
+  }
+
   return (
     <>
-      <h1 className='contact'> Thanks for taking the time to reach out. How can I help you today?</h1>
+      <h1>Thanks for taking the time to reach out. How can I help you today?</h1>
+      <form onSubmit={handleSubmit} className='emailForm'>
+        <input
+          type='text'
+          placeholder='Your name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type='email'
+          placeholder='Your email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          cols='30'
+          rows='10'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        > </textarea>
+        <button type="btn"> Submit </button>
+      </form>
     </>
-
   )
 }
