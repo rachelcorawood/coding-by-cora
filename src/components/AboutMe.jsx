0a0ce0427f8d4ee3c568f9717f02f53a71e55559
Cora <115031670+rachelcorawood@users.noreply.github.com>
2023-12-12 18:19:58 -0800

    fixed problem

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -21,7 +21,8 @@ export default function AboutMe() {
         <h1 className='bio-title'> Hi, I'm Cora. Nice to meet you.</h1>
         <h2 className='bio-paragraph'>Prior to freelancing as a Web Developer, I was an Administrative Assistant and Medical Receptionist for a celebrity-acclaimed plastic surgeon. This work required extreme organization and obsessive attention to detail because one wrong mistake on a medical file could lead to a potentially fatal error. These skills of being precise and solution-oriented under pressure would make me an ideal candidate to handle building your site with preparedness and ease.<br></br>
           <br></br>
-          {/* I've also acted and produced film for over fourteen years. I dealt with continuous hurdles and was nicknamed “the rock” for instilling patience in our cast, crew, and investors. My resourcefulness and problem-solving mindset, has helped numerous creative projects surpass the finish line and I'd love to help yours. </h2> */}
+          {/* I've also acted and produced film for over fourteen years. I dealt with continuous hurdles and was nicknamed “the rock” for instilling patience in our cast, crew, and investors. My resourcefulness and problem-solving mindset, has helped numerous creative projects surpass the finish line and I'd love to help yours. */}
+          </h2>
       </div>
     </>
 
