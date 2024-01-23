736368f1f1e1dc8000125aebfe761a21041b417f
Cora <115031670+rachelcorawood@users.noreply.github.com>
2024-01-22 18:02:44 -0800

    fixed spacing

--- src/data/data.js
+++ src/data/data.js
@@ -18,7 +18,7 @@ export const people = [
   },
   {
     id: 3,
-    image: "https://media.licdn.com/dms/image/D4E03AQGdRXyFYcvZEA/profile-displayphoto-shrink_800_800/0/1664483636985?e=1704931200&v=beta&t=kPnzHGKqAjB72BR32AJAmFfHaJbteXsFjmIZh09qtUg",
+    image: "https://media.licdn.com/dms/image/D4E03AQGdRXyFYcvZEA/profile-displayphoto-shrink_400_400/0/1664483636985?e=1711584000&v=beta&t=WAUleIihWqxfn-ZO8tpPhqfdL4AAuPSH6RU4r7d0SHE",
     name: "Mae May",
     title: "Software Engineer, New York, NY",
     quote: "I have had the pleasure to both work alongside Cora as an engineer, and to witness her ability to bring concepts into reality as a film producer as well. As an engineer, Cora is a pleasure to work with, and has a great ability to ask the right questions to solve problems, and works efficiently and resourcefully to get things done. She is as skilled with coding as she is with the social dynamics of collaboratively building applications, and makes even the most challenging projects enjoyable. I cannot recommend her enough!"
