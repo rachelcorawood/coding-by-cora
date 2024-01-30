b0473976419df75f33f495843f6e083a8dc69d80
Cora <115031670+rachelcorawood@users.noreply.github.com>
2024-01-29 16:31:11 -0800

    Pitch video added to site

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,6 @@
 import './AboutMe.css'
 import photo from '../assets/portfolio-photo.png'
-
+import ReactPlayer from 'react-player'
 
 
 
@@ -12,8 +12,8 @@ export default function AboutMe() {
 
       <h3 className='title-description'>I code and design simple beautiful sites to help those in health, wellness, and the arts reach their audiences.</h3>
 
-      <div className='image'>
-        <img src={photo} alt="portfolio-photo" />
+      <div className='video'>
+        <ReactPlayer controls={true} url="https://vimeo.com/907654075?share=copy#t=0" alt='portfolio-video' />
       </div>
 
 
@@ -22,7 +22,7 @@ export default function AboutMe() {
         <h2 className='bio-paragraph'>Prior to freelancing as a Web Developer, I was an Administrative Assistant and Medical Receptionist for a celebrity-acclaimed plastic surgeon. This work required extreme organization and obsessive attention to detail because one wrong mistake on a medical file could lead to a potentially fatal error. These skills of being precise and solution-oriented under pressure would make me an ideal candidate to handle building your site with preparedness and ease.<br></br>
           <br></br>
           {/* I've also acted and produced film for over fourteen years. I dealt with continuous hurdles and was nicknamed “the rock” for instilling patience in our cast, crew, and investors. My resourcefulness and problem-solving mindset, has helped numerous creative projects surpass the finish line and I'd love to help yours. */}
-          </h2>
+        </h2>
       </div>
     </>
 
