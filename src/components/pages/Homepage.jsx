d7ca70954989cde3b0f08bb9f79ae2a49692561f
Cora <115031670+rachelcorawood@users.noreply.github.com>
2025-01-20 17:49:49 -0800

    added testimonials

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -12,8 +12,8 @@ export default function Homepage() {
     <>
       <AboutMe />
       <Project />
-      {/* <Skill />
       <Testimonial />
+      {/* <Skill />
       <Startups />
       <CallToAction />
       <Footer/> */}
